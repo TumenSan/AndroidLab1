d1b26a742cb39c88ec98a0969671b16fbd46cf7b
TumenSan <sandanov.tumen.sib@mail.ru>
2023-02-26 03:30:41 +0700

    com 2

--- app/src/main/java/com/example/lab1/DescrtiptionPage1.kt
+++ app/src/main/java/com/example/lab1/DescrtiptionPage1.kt
@@ -1,18 +1,18 @@
 package com.example.lab1
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class DescriptionPage1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.description_page1)
     }
 
     public fun nextPage(view: View){
-        val intent = Intent(this, DescriptionPage1::class.java)
+        val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
     }
 }
