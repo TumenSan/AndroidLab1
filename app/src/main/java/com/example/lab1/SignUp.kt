d4f61284209b965667d1f3bcd0f0fdb4029370f0
TumenSan <sandanov.tumen.sib@mail.ru>
2023-03-03 02:39:36 +0700

    com 2 signup

--- app/src/main/java/com/example/lab1/SignUp.kt
+++ app/src/main/java/com/example/lab1/SignUp.kt
@@ -16,12 +16,14 @@ class SignUp : AppCompatActivity() {
         // get reference to all views
         var et_user_name = findViewById(R.id.et_user_name) as EditText
         var et_password = findViewById(R.id.et_password) as EditText
+        var et_email = findViewById(R.id.et_email) as EditText
         var btn_submit = findViewById(R.id.btn_submit) as Button
 
         // set on-click listener
         btn_submit.setOnClickListener {
             val user_name = et_user_name.text;
             val password = et_password.text;
+            val email = et_email.text;
             Toast.makeText(this@SignUp, user_name, Toast.LENGTH_LONG).show()
 
             // your code to validate the user_name and password combination
