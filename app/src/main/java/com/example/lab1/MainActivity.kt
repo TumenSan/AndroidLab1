2cd9594e8ee4f5467a14fc6e3640511824c0ed25
TumenSan <sandanov.tumen.sib@mail.ru>
2023-03-05 13:10:19 +0700

    com 7 splash screen

--- app/src/main/java/com/example/lab1/MainActivity.kt
+++ app/src/main/java/com/example/lab1/MainActivity.kt
@@ -3,16 +3,20 @@ package com.example.lab1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.os.Handler
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+            val intent = Intent(this@MainActivity, DescriptionPage1::class.java)
+            startActivity(intent)
+        }, 2000)
     }
 
-    public fun nextPage(view: View){
-        val intent = Intent(this, DescriptionPage1::class.java)
-        startActivity(intent)
-    }
 }
