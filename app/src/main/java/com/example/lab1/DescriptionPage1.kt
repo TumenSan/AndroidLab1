5bf8df2952597f96df893b61d161a092ea7be11c
TumenSan <sandanov.tumen.sib@mail.ru>
2023-03-05 15:20:21 +0700

    com 8 dots

--- app/src/main/java/com/example/lab1/DescriptionPage1.kt
+++ app/src/main/java/com/example/lab1/DescriptionPage1.kt
@@ -4,11 +4,22 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.ViewPager
+import com.tbuonomo.viewpagerdotsindicator.DotsIndicator
 
 class DescriptionPage1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.description_page1)
+
+        /*
+        val dotsIndicator = findViewById<DotsIndicator>(R.id.dots_indicator)
+        val viewPager = findViewById<ViewPager>(R.id.view_pager)
+        val adapter = ViewPagerAdapter()
+        viewPager.adapter = adapter
+        dotsIndicator.attachTo(viewPager)
+
+         */
     }
 
     public fun nextPage(view: View){
